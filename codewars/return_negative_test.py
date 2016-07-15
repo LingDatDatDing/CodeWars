2fc2faa1b8e49695077de2732d3cc371fbedb322
Ollie Ling <lingollie101@gmail.com>
2016-07-15 11:52:50 +0100

    Will broke it

--- codewars/return_negative_test.py
+++ codewars/return_negative_test.py
@@ -1,9 +1,9 @@
 from nose.tools import assert_equal as assertEqual
-from return_negative import make_negative
+import return_negative
 
 def test_make_negative_large_negative():
-    assertEqual(make_negative(42),-42)
+    assertEqual(return_negative.make_negative(42),-42)
 def test_make_negative_small_negative():
-    assertEqual(make_negative(-9),-9)
+    assertEqual(return_negative.make_negative(-9),-9)
 def test_make_negative_zero():
-    assertEqual(make_negative(0),0)
+    assertEqual(return_negative.make_negative(0),0)
